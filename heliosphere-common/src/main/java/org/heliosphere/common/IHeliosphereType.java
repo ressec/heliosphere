747a97428612d932e8d2cbfba058c255bf1172d7
Resse Christophe <christophe.resse@gmail.com>
2017-11-03 16:30:32 +0100

    Added marker interface.

---
+++ heliosphere-common/src/main/java/org/heliosphere/common/IHeliosphereType.java
@@ -0,0 +1,12 @@
+package org.heliosphere.common;
+
+/**
+ * Marker interface for the {@code Heliosphere} types.
+ * <hr>
+ * @author  <a href="mailto:christophe.resse@gmail.com">Resse Christophe - Heliosphere</a>
+ * @version 1.0.0
+ */
+public interface IHeliosphereType
+{
+	// Empty.
+}
