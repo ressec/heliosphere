37d5e78938bfa25decd6d1b6f7e6ea86550f0470
Resse Christophe <christophe.resse@gmail.com>
2017-11-15 09:45:52 +0100

    Added some fake data quick tests.

---
+++ heliosphere-geocoding/src/main/java/org/heliosphere/geocoding/sample/TestFakeChuckNorris.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright(c) 2017 - Heliosphere Corp.
+ * ---------------------------------------------------------------------------
+ * This file is part of the Heliosphere's project which is licensed under the 
+ * Apache license version 2 and use is subject to license terms.
+ * You should have received a copy of the license with the project's artifact
+ * binaries and/or sources.
+ * 
+ * License can be consulted at http://www.apache.org/licenses/LICENSE-2.0
+ * ---------------------------------------------------------------------------
+ */
+package org.heliosphere.geocoding.sample;
+
+import java.util.Locale;
+
+import com.github.javafaker.Faker;
+
+/**
+ * A sample Java application that generates a set of fake phone numbers followed
+ * by a set of fake cell numbers in a pre-defined language.
+ * <hr>
+ * @author <a href="mailto:christophe.resse@gmail.com">Christophe Resse - Heliosphere</a>
+ * @version 1.0.0
+ */
+public class TestFakeChuckNorris
+{	
+	/**
+	 * Java application main-entry point.
+	 * <p>
+	 * @param arguments Arguments passed on the command line.
+	 */
+	@SuppressWarnings("nls")
+	public static void main(String[] arguments)
+	{
+		Faker faker = new Faker(Locale.FRENCH);
+		StringBuilder fact = null;
+		
+		for (int i = 0; i < 10; i++)
+		{
+			fact = new StringBuilder(faker.chuckNorris().fact());
+			System.out.println(String.format("Fact: %1s", fact));
+		}
+	}
+}
---
+++ heliosphere-geocoding/src/main/java/org/heliosphere/geocoding/sample/TestFakePhoneNumber.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright(c) 2017 - Heliosphere Corp.
+ * ---------------------------------------------------------------------------
+ * This file is part of the Heliosphere's project which is licensed under the 
+ * Apache license version 2 and use is subject to license terms.
+ * You should have received a copy of the license with the project's artifact
+ * binaries and/or sources.
+ * 
+ * License can be consulted at http://www.apache.org/licenses/LICENSE-2.0
+ * ---------------------------------------------------------------------------
+ */
+package org.heliosphere.geocoding.sample;
+
+import java.util.Locale;
+
+import com.github.javafaker.Faker;
+
+/**
+ * A sample Java application that generates a set of fake phone numbers followed
+ * by a set of fake cell numbers in a pre-defined language.
+ * <hr>
+ * @author <a href="mailto:christophe.resse@gmail.com">Christophe Resse - Heliosphere</a>
+ * @version 1.0.0
+ */
+public class TestFakePhoneNumber
+{	
+	/**
+	 * Java application main-entry point.
+	 * <p>
+	 * @param arguments Arguments passed on the command line.
+	 */
+	@SuppressWarnings("nls")
+	public static void main(String[] arguments)
+	{
+		Faker faker = new Faker(Locale.FRENCH);
+		StringBuilder phone = null;
+		
+		for (int i = 0; i < 10; i++)
+		{
+			phone = new StringBuilder(faker.phoneNumber().phoneNumber());
+			System.out.println(String.format("Phone number: %1s", phone));
+		}
+
+		for (int i = 0; i < 10; i++)
+		{
+			phone = new StringBuilder(faker.phoneNumber().cellPhone());
+			System.out.println(String.format("Cell number: %1s", phone));
+		}
+	}
+}
