bc0f289d0c043df6d9c37cebba7482cbc96a10de
Resse Christophe <christophe.resse@gmail.com>
2018-01-15 13:16:09 +0100

    Added command entities.

---
+++ heliosphere-common/src/main/java/org/heliosphere/common/command/CommandInterpreter.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright(c) 2017-2017 Heliosphere Corp.
+ * ---------------------------------------------------------------------------
+ * This file is part of the Heliosphere's project which is licensed under the
+ * Apache license version 2 and use is subject to license terms.
+ * You should have received a copy of the license with the project's artifact
+ * binaries and/or sources.
+ * 
+ * License can be consulted at http://www.apache.org/licenses/LICENSE-2.0
+ * ---------------------------------------------------------------------------
+ */
+package org.heliosphere.common.command;
+
+import lombok.extern.log4j.Log4j;
+
+/**
+ * A command interpreter is responsible to interpret a text entered on the command line and to
+ * transform it into an execution command.
+ * <hr>
+ * @author <a href="mailto:christophe.resse@gmail.com">Christophe Resse - Heliosphere</a>
+ * @version 1.0.0
+ */
+@Log4j
+public class CommandInterpreter
+{
+	// Empty.
+}
---
+++ heliosphere-common/src/main/java/org/heliosphere/common/command/CommandValidator.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright(c) 2017-2017 Heliosphere Corp.
+ * ---------------------------------------------------------------------------
+ * This file is part of the Heliosphere's project which is licensed under the
+ * Apache license version 2 and use is subject to license terms.
+ * You should have received a copy of the license with the project's artifact
+ * binaries and/or sources.
+ * 
+ * License can be consulted at http://www.apache.org/licenses/LICENSE-2.0
+ * ---------------------------------------------------------------------------
+ */
+package org.heliosphere.common.command;
+
+import lombok.extern.log4j.Log4j;
+
+/**
+ * A command validator is responsible to validate execution commands according to a set of pre-defined rules.
+ * <hr>
+ * @author <a href="mailto:christophe.resse@gmail.com">Christophe Resse - Heliosphere</a>
+ * @version 1.0.0
+ */
+@Log4j
+public class CommandValidator
+{
+	// Empty.
+}
